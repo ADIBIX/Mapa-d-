a0ea3fb11e3de3bad1b0401ffcba6bd925067452
R3vii <revi.pingwin@gmail.com>
2025-07-09 18:22:35 +0200

    Update markersData.js

--- markersData.js
+++ markersData.js
@@ -5,5 +5,14 @@ var markersData = [
     { id: 'd 3', lat: 1511.56, lng: 3334.06, title: 'D? Złom', imageUrl: 'Złomiarz.png', countAs: 1 },
     { id: 'd 4', lat: 3791.63, lng: 4756.75, title: 'D? Szopa', imageUrl: 'Szopa.png', countAs: 1 },
     { id: 'd 5', lat: 1857.63, lng: 4218.38, title: 'D? tania lakiernia-skatepark', imageUrl: '', countAs: 1 },
-    { id: 'd 5', lat: 697.50, lng: 4572.75, title: 'D? Statek', imageUrl: '', countAs: 1 },
+    { id: 'd 6', lat: 697.50, lng: 4572.75, title: 'D? Statek', imageUrl: '', countAs: 1 },
+
+
+
+    
+    //Zrobione przez R3vii
+    { id: 'd 7', lat: 2077.11, lng: 3447.08, title: 'D? Budowa', imageUrl: '', countAs: 1 },
+    { id: 'd 8', lat: 5074.50, lng: 3602.00, title: 'D? Silosy', imageUrl: '', countAs: 1 },
+
+    
 ]; 
