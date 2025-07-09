9ff4404ef67fae371a68447160528db9cfe81470
ADIBIX <adibix5@gmail.com>
2025-07-09 14:40:40 +0200

    Add files via upload

---
+++ markersData.js
@@ -0,0 +1,7 @@
+// markersData.js
+var markersData = [
+    { id: 'd 1', lat: 1700.50, lng: 2999.00, title: 'D? Sadownik', imageUrl: 'sad.png', countAs: 1 },
+    { id: 'd 2', lat: 5804.88, lng: 3553.50, title: 'D? Tunel', imageUrl: 'DilerTunel.png', countAs: 1 },
+    { id: 'd 3', lat: 1511.56, lng: 3334.06, title: 'D? Złom', imageUrl: 'Złomiarz.png', countAs: 1 },
+    { id: 'd 4', lat: 3791.63, lng: 4756.75, title: 'D? Szopa', imageUrl: 'Szopa.png', countAs: 1 },
+]; 
